84e960fe45b27912cebfb3c5f9cb381748d34237
taylrzhang <neil0725@hotmail.com>
2022-03-14 23:37:54 -0700

    drawer added

---
+++ zhang.taylr/js/app.js
@@ -0,0 +1,40 @@
+
+$(() => {
+    checkUserId();
+ 
+    // EVENT DELEGATION
+    $(document)
+ 
+    // FORM SUBMISSIONS
+    .on("submit", "#signin-form", function(e) {
+       e.preventDefault();
+       checkLoginForm();
+    })
+ 
+ 
+    // CLICKS
+    .on("click", ".js-logout", function() {
+       sessionStorage.removeItem("userId");
+       checkUserId();
+    })
+ 
+ 
+    // ACTIVATE TOOLS
+    .on("click", "[data-activate]", function() {
+        let target = $(this).data("activate");
+        $(target).addClass("active")
+     })
+     .on("click", "[data-deactivate]", function() {
+        let target = $(this).data("deactivate");
+        $(target).removeClass("active")
+     })
+     .on("click", "[data-toggle]", function() {
+        let target = $(this).data("toggle");
+        $(target).toggleClass("active")
+     })
+     .on("click", "[data-activateone]", function() {
+        let target = $(this).data("activate");
+        $(target).addClass("active")
+           .siblings().removeClass("active")
+     })
+ });
---
+++ zhang.taylr/js/signin.js
@@ -0,0 +1,35 @@
+
+const checkLoginForm = () => {
+    let user = $("#signin-username").val();
+    let pass = $("#signin-password").val();
+ 
+    console.log(user,pass)
+ 
+    if (user === 'user' && pass === 'pass') {
+       // logged in
+       console.log('success');
+       sessionStorage.userId = 3;
+       $("#signin-form")[0].reset();
+    } else {
+       // not logged in
+       console.log('failure');
+       sessionStorage.removeItem('userId');
+    }
+ 
+    checkUserId();
+ }
+ 
+ const checkUserId = () => {
+    let p = ["#signin-page","#signup-page",""];
+ 
+    if (sessionStorage.userId === undefined) {
+       // not logged in
+       if(!p.some(o => o === window.location.hash))
+          $.mobile.navigate("#signin-page");
+    } else {
+       // logged in
+       if(p.some(o => o === window.location.hash))
+          $.mobile.navigate("#explore-page");
+          
+    }
+ }
